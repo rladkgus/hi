d9c8d98982d0dd686ff690a15d2a28b2633b3f17
rladkgus1914 <rladkgus1914@naver.com>
2020-05-18 17:56:18 +0900

    20200518

---
+++ Nongshim3/Nongshim2/assets/js/custom.js
@@ -0,0 +1,11 @@
+
+var mySwiper2 = new Swiper ('#nav .swiper-container', {
+   // Optional parameters
+   slidesPerView : 'auto',
+   // If we need pagination
+   scrollbar: {
+     el: '.swiper-scrollbar',
+     dragSize : 152,
+     hide: false,
+   },
+ });
